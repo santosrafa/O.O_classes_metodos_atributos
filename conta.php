752d17d68529bb1bf0b145e99cdd1498d9a720e0
Your Name <ti.rafaelteles@gmail.com>
2022-08-21 19:45:04 -0300

    first commit

---
+++ conta.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Classe Conta */
+
+//Essas variáveis são chamadas de atributos da Conta ou objetos
+//-> Esse simbolo significa que estou acessando um atributo
+//public :: Significa que este dado será publico
+
+class Conta                 
+{
+    public $cpfTitular;                 
+    public $nomeTitular;
+    public $saldo;
+}
