b3a098fc77a7b6387da4541174cdfa335c88e984
Your Name <ti.rafaelteles@gmail.com>
2022-08-24 20:02:22 -0300

    conta

--- conta.php
+++ conta.php
@@ -5,20 +5,42 @@
 //Essas variáveis são chamadas de atributos da Conta ou objetos
 //-> Esse simbolo significa que estou acessando um atributo
 //public :: Significa que este dado será publico
+//Classe é a forma do bolo e objeto é bolo feito.
 
 class Conta                 
 {
     public $cpfTitular;                 
     public $nomeTitular;
-    public $saldo;
+    public $saldo = 0;              
 
     public function sacar (float $valorASacar)                  //Uma função que está dentro de uma classe é chamada de método
     {
         if ($valorASacar > $this->saldo){
             echo "Saldo indisponivel";
-        }else{
-            $this->saldo -= $valorASacar;
+            return; 
         }
+        
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar (float $valorADepositar) : void    //void: esse metodo nao devolve nada pra gente nao tem retorno
+    {
+        if ($valorADepositar < 0){
+            echo "Valor precisa ser positivo";
+            return;                                             //return :: quando o metodo chegar no return ele para a execução
+        }
+        
+        $this->saldo += $valorADepositar;  
+    }
+
+    public function transferir (float $valorATransferir, Conta $contaDestino) : void
+    {
+        if ($valorATransferir > $this->saldo){
+            echo "Saldo Indisponivel!";
+            return;
+        }
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
     }
 }
 
