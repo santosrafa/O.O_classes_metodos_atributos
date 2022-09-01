3ab653af3940d682216a779d2806350aedd9c1b4
Your Name <ti.rafaelteles@gmail.com>
2022-09-01 20:11:19 -0300

    conta

--- banco.php
+++ banco.php
@@ -2,10 +2,12 @@
 
 require_once 'conta.php';
 
-$primeiraConta = new Conta('123.456.789-10', 'Rafael Teles');
+$primeiraConta = new Conta('123.456.789-10', 'Rafael');
 $primeiraConta->depositar(500);
 $primeiraConta->sacar(300);
 
 echo $primeiraConta->recuperarNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperarSaldo() . PHP_EOL;          //Chamando o metodo recuperar saldo.
 echo $primeiraConta->recuperarCpfTitular() . PHP_EOL;
+
+echo Conta::recuperaNumeroDeContas();
--- conta.php
+++ conta.php
@@ -8,21 +8,25 @@
 //Classe é a forma do bolo e objeto é bolo feito.
 //É uma boa pratica colocar as propriedades sempre privadas e os metodos publicos
 
+
 class Conta                 
 {
     private $cpfTitular;                 
     private $nomeTitular;
     private $saldo;            
+    private static $numeroDeContas = 0;                                      //Membros estáticos. São membros da classe em si, e não de cada instância (objeto).
 
-    public function __construct(string $cpfTitular, string $nomeTitular)  //Utilizado para qualquer inicialização que o objeto necessite antes de ser utilizado.
+    public function __construct(string $cpfTitular, string $nomeTitular)     //Utilizado para qualquer inicialização que o objeto necessite antes de ser utilizado.
     {
         $this->cpfTitular = $cpfTitular;
         $this->validaNomeTitular($nomeTitular);
         $this->nomeTitular = $nomeTitular;
         $this->saldo = 0;
+        
+        self::$numeroDeContas++;
     }
 
-    public function sacar (float $valorASacar)                  //Uma função que está dentro de uma classe é chamada de método
+    public function sacar (float $valorASacar)                                //Uma função que está dentro de uma classe é chamada de método
     {
         if ($valorASacar > $this->saldo){
             echo "Saldo indisponivel";
@@ -32,11 +36,11 @@ class Conta
         $this->saldo -= $valorASacar;
     }
 
-    public function depositar (float $valorADepositar) : void    //void: esse metodo nao devolve nada pra gente nao tem retorno
+    public function depositar (float $valorADepositar) : void                  //void: esse metodo nao devolve nada pra gente nao tem retorno
     {
         if ($valorADepositar < 0){
             echo "Valor precisa ser positivo";
-            return;                                             //return :: quando o metodo chegar no return ele para a execução
+            return;                                                             //return :: quando o metodo chegar no return ele para a execução
         }
         
         $this->saldo += $valorADepositar;  
@@ -52,7 +56,7 @@ class Conta
         $contaDestino->depositar($valorATransferir);
     }
 
-    public function recuperarSaldo(): float                    //Criando metodos para acessar as propriedades.
+    public function recuperarSaldo(): float                                        //Criando metodos para acessar as propriedades.
     {
         return $this->saldo;
     }
@@ -67,12 +71,17 @@ class Conta
         return $this->nomeTitular;
     }
 
-    private function validaNomeTitular(string $nomeTitular)         //Metodo valida nome.
+    private function validaNomeTitular(string $nomeTitular)                         //Metodo valida nome.
     {
         if (strlen($nomeTitular) < 5){
             echo "Nome precisa ter no minimo 5 caracteres";
             exit();
         }
+    }
+
+    public static function recuperaNumeroDeContas(): int
+    {
+        return self::$numeroDeContas;
     }
 }
 
